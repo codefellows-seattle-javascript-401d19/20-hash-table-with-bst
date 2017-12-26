d76ba9ee3a22e64819102e732f9dd37a981651d7
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-25 16:29:25 -0800

    update hash function again

---
+++ lab-shannon/README.md
@@ -0,0 +1,1 @@
+Hash function credit to http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
--- lab-shannon/model/hash-table.js
+++ lab-shannon/model/hash-table.js
@@ -15,13 +15,13 @@ class HashTable{
 
     for(let i in key){
       char = key.charCodeAt(i);
-      hash += char*Math.pow(31, keyLength - i)   // this seems like it's based on Java implementation
-      hash = hash & hash;   // from MDN: "Bitwise AND	a & b	Returns a 1 in each bit position for which the corresponding bits of both operands are 1's." seems to make output 32bit integer
-      // hash = ((hash << 5) - hash) + char;    // this seems to be the JS way, but I don't understand bit shifting
-      // hash |= 0;
+      // hash += char*Math.pow(31, keyLength - i)   // this seems like it's based on Java implementation
+      hash = ((hash << 5) - hash) + char;    // this seems to be the JS way, but I don't understand the bit shifting well
+      hash = hash & hash;   // from MDN: "Bitwise AND;	a & b	Returns a 1 in each bit position for which the corresponding bits of both operands are 1's." seems to make output 32bit integer
     }
     return hash % this._capacity;
   }
+  
   set(key){}
   get(key){}
   remove(key){}
