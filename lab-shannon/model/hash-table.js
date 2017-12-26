311930103e0b55c97bf026dac3cca0523d75fbdd
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-25 16:55:53 -0800

    fix linter errors

--- lab-shannon/model/hash-table.js
+++ lab-shannon/model/hash-table.js
@@ -12,11 +12,11 @@ class HashTable{
     if(typeof key !== 'string'){
       throw new TypeError(`Key value must be a string`);
     }
-    let keyLength = key.length;
+    // let keyLength = key.length;
     let hash = null;
 
     for(let i in key){
-      char = key.charCodeAt(i);
+      let char = key.charCodeAt(i);
       // hash += char*Math.pow(31, keyLength - i)   // this seems like it's based on Java implementation
       hash = ((hash << 5) - hash) + char;    // this seems to be the JS way, but I don't understand the bit shifting well
       hash = hash & hash;   // from MDN: "Bitwise AND;	a & b	Returns a 1 in each bit position for which the corresponding bits of both operands are 1's." seems to make output 32bit integer
@@ -32,7 +32,7 @@ class HashTable{
       return this;
     }
 
-    let node = this._buckets[hash].find(node => node.key.value === value);
+    let node = this._buckets[hash].find(node => node.key.value === key);
     if(node){
       node.value.HTvalue = HTvalue;
       return this;
