dd7e3cad2952763e426f4edcf6c032111205990e
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-25 16:25:16 -0800

    revise hash function

--- lab-shannon/model/hash-table.js
+++ lab-shannon/model/hash-table.js
@@ -11,10 +11,14 @@ class HashTable{
       throw new TypeError(`Key value must be a string`);
     }
     let keyLength = key.length;
+    let hash = null;
 
     for(let i in key){
       char = key.charCodeAt(i);
-      hash += char*Math.pow(31, keyLength - i)
+      hash += char*Math.pow(31, keyLength - i)   // this seems like it's based on Java implementation
+      hash = hash & hash;   // from MDN: "Bitwise AND	a & b	Returns a 1 in each bit position for which the corresponding bits of both operands are 1's." seems to make output 32bit integer
+      // hash = ((hash << 5) - hash) + char;    // this seems to be the JS way, but I don't understand bit shifting
+      // hash |= 0;
     }
     return hash % this._capacity;
   }
