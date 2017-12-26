3606f4a931f8cce810a04211922a27a9f97c0e6e
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-25 16:02:49 -0800

    write hash function

--- lab-shannon/model/hash-table.js
+++ lab-shannon/model/hash-table.js
@@ -5,6 +5,22 @@ class HashTable{
     this._capacity = capacity;
     this._buckets = new Array(capacity);
   }
+
+  _hash(key){
+    if(typeof key !== 'string'){
+      throw new TypeError(`Key value must be a string`);
+    }
+    let keyLength = key.length;
+
+    for(let i in key){
+      char = key.charCodeAt(i);
+      hash += char*Math.pow(31, keyLength - i)
+    }
+    return hash % this._capacity;
+  }
+  set(key){}
+  get(key){}
+  remove(key){}
 }
 
 module.exports = HashTable;
