5cb1ade256badd3e4810533d6beccacd64a6c100
David Lindahl <davidalindahl@gmail.com>
2017-12-28 09:16:56 -0800

    filled out constructor for bs tree

--- lab-david/lib/binary-search-tree.js
+++ lab-david/lib/binary-search-tree.js
@@ -1,1 +1,15 @@
 'use strict';
+
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value)
+
+  remove(value)
+
+  
+}
