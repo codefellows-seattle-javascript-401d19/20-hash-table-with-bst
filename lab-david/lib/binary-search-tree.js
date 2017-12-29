142d25f73e449c077100ca5f160c5a69d9488ef5
David Lindahl <davidalindahl@gmail.com>
2017-12-28 19:52:35 -0800

    filled in binary search tree from my other assignment

--- lab-david/lib/binary-search-tree.js
+++ lab-david/lib/binary-search-tree.js
@@ -1,15 +1,54 @@
 'use strict';
 
-class BinarySearchTree {
-  constructor(value) {
+class BinarySearchTree{
+  constructor(value){
     this.value = value;
     this.left = null;
     this.right = null;
   }
 
-  insert(value)
+  insert(value){
+    if(typeof !== 'number')
+      throw new TypeError('BST value should be a number');
+    
+      if(this.value === value)
+        throw new Error('BST - value is already present');
 
-  remove(value)
+      if(value < this.value){
+        if(!this.left){
+          this.left = new BinarySearchTree(value);
+          return;
+        }
+        this.left.insert(value);
+        return;
+      }
+      if(!this.right){
+        this.right = new BinarySearchTree(value);
+        return;
+      }
+      this.right.insert(value);
+      return;
+  }
 
-  
+  find(value){
+    if(value === this.value)
+      return this;
+
+    if(value > this.value){
+      if(this.right !== null)
+        return this.right.find(value);
+      else 
+        return null
+    }
+    if(this.left !== null)
+      return this.left.find(value);
+    else
+      return null;
+  }
 }
+
+let bst = new BinarySearchTree(10);
+bst.insert(5);
+bst.insert(2);
+bst.insert(8);
+bst.insert(16);
