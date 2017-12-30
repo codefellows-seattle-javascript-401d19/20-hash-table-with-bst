2e9bf6b281fa9dfd4e918415cdee571ff60d0c89
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-12-30 10:48:57 -0800

    hash table set works

--- lab-nicholasc/__test__/hash-table.test.js
+++ lab-nicholasc/__test__/hash-table.test.js
@@ -2,65 +2,22 @@
 
 const HashTable = require('../model/hash-table');
 
-const BST = require('../model/search-tree');
+describe('/model/hash-table', () => {
+  let hashTable = new HashTable();
+  hashTable.set('one', 1);
+  hashTable.set('two', 2);
+  hashTable.set('three', 3);
+  hashTable.set('four', 4);
+  hashTable.set('i', 44);
+  hashTable.set('declare', 32);
+  hashTable.set('a', 34);
+  hashTable.set('thumb', 32);
+  hashTable.set('war', 32);
 
-
-describe('/lib/search-tree', () => {
-
-
-
-  let notATree = new BST(10);
-  notATree.value = null;
-
-  describe('Binary Search Tree', () => {
-    test.only('remove', () => {
-      let mockBST = new BST();
-      mockBST.insert(9, 9);
-      mockBST.insert(8, 8);
-      mockBST.insert(10, 10);
-      mockBST.insert(12, 12);
-      mockBST.insert(11, 11);
-      mockBST.insert(15, 15);
-      console.log(mockBST);
-      console.log(mockBST.toString());
-      expect(mockBST.toString()).toEqual(' 8  9  10  11  12  15 ');
-      mockBST.remove(12);
-      expect(mockBST.toString()).toEqual(' 8  9  10  11  15 ');
-    });
-    test('remove from null', () => {
-      let nullBST = new BST();
-      nullBST.value = null;
-      expect(nullBST.remove(9)).toEqual(null);
-    });
-    //TODO: remove specific things
-    test('remove head', () => {
-      let headTree = new BST();
-      headTree.insert(10);
-      headTree.insert(8);
-      headTree.insert(12);
-      headTree.insert(11);
-      headTree.insert(15);
-    });
-
-    test('remove right leaf', () => {
-      let rightTree = new BST();
-      rightTree.insert(10);
-      rightTree.insert(8);
-      rightTree.insert(12);
-      expect(rightTree.right.value).toEqual(12);
-      rightTree.remove(12);
-      expect(rightTree.right).toEqual(null);
-    });
-
-    test('remove node with left and right', () => {
-      let testTree = new BST(10);
-      testTree.insert(8);
-      testTree.insert(12);
-      testTree.insert(11);
-      testTree.insert(15);
-      expect(testTree.right.value).toEqual(12);
-      testTree.remove(12);
-      expect(testTree.right.value).toEqual(15);
+  describe('set', () => {
+    test('set should add nodes to the Hash Table', () => {
+      hashTable.set('five', 5);
+      expect(hashTable._buckets[hashTable._generateHash('five')].root.value).toEqual(5);
     });
   });
 });
